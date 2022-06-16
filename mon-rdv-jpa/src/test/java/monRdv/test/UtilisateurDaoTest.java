2b362a5e7eee568bdf907ec71b8acd8666d7313c
unknown <antoine.chou@harmonicinc.com>
2022-06-16 14:46:50 +0200

    Utilisateur DAO test

---
+++ mon-rdv-jpa/src/test/java/monRdv/test/UtilisateurDaoTest.java
@@ -0,0 +1,84 @@
+package monRdv.test;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+import monRdv.Singleton;
+import monRdv.dao.IUtilisateurDao;
+import monRdv.model.Civilite;
+import monRdv.model.Patient;
+import monRdv.model.Praticien;
+
+public class UtilisateurDaoTest {
+
+    IUtilisateurDao utilisateurDao = Singleton.getInstance().getUtilisateurDao();
+
+    @Test
+    public void patient() {
+        int sizeStart = utilisateurDao.findAll().size();
+
+        Patient dupont = new Patient(Civilite.M, "DUPONT", "Pierre");
+        
+		dupont.setEmail("pierre.dupont@gmail.com");
+		dupont.setMotDePasse("P@assword");
+		dupont.setAge(35);
+		dupont.setNumeroSS("1 85 33 14 745");
+		dupont.setTelephone("06 06 06 06 06");
+
+        dupont = (Patient) utilisateurDao.save(dupont);
+
+        Patient testPatient = (Patient) utilisateurDao.findById(dupont.getId());
+
+        Assert.assertNotNull(testPatient);
+
+        Assert.assertEquals(Civilite.M, testPatient.getCivilite());
+        Assert.assertEquals("DUPONT", testPatient.getNom());
+        Assert.assertEquals("Pierre", testPatient.getPrenom());
+        Assert.assertEquals("pierre.dupont@gmail.com", testPatient.getEmail());
+        Assert.assertEquals("P@assword", testPatient.getMotDePasse());
+        Assert.assertEquals(35, testPatient.getAge());
+        Assert.assertEquals("1 85 33 14 745", testPatient.getNumeroSS());
+        Assert.assertEquals("06 06 06 06 06", testPatient.getTelephone());
+
+        Assert.assertEquals( utilisateurDao.findAll().size() - sizeStart, 1);
+
+        utilisateurDao.delete(testPatient);
+
+        Assert.assertEquals(sizeStart, utilisateurDao.findAll().size());
+    }
+
+    @Test
+    public void practicien() {
+        int sizeStart = utilisateurDao.findAll().size();
+
+        Praticien jekyll = new Praticien("JEKYLL", "Henri");
+		jekyll.setEmail("dr.jekyll@gmail.com");
+		jekyll.setMotDePasse("Hyde");
+		jekyll.setMatricule("8888888");
+
+        jekyll = (Praticien) utilisateurDao.save(jekyll);
+
+        Praticien hyde = (Praticien) utilisateurDao.findById(jekyll.getId());
+
+        Assert.assertNotNull(hyde);
+            
+        Assert.assertEquals(hyde.getNom(), "JEKYLL");
+        Assert.assertEquals(hyde.getPrenom(), "Henri");
+        Assert.assertEquals(hyde.getMotDePasse(), "Hyde");
+        Assert.assertEquals(hyde.getEmail(), "dr.jekyll@gmail.com");
+        Assert.assertEquals(hyde.getMatricule(), "8888888");
+
+
+        Assert.assertEquals( utilisateurDao.findAll().size() - sizeStart, 1);
+
+        utilisateurDao.delete(hyde);
+
+        Assert.assertEquals(sizeStart, utilisateurDao.findAll().size());
+    }
+
+    @Test
+    public void administrateur() {
+    }
+
+}
